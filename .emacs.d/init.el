a6cb5ed121f25d950944e4b4143405b141ebba7f
Takumi Abe <abe@engraphia.com>
2015-05-13 15:41:46 +0900

    add graphbiz-mode to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -44,6 +44,7 @@
     web-mode
     csv-mode
     coffee-mode
+    graphviz-dot-mode
     haml-mode
     php-mode
     scss-mode
@@ -189,6 +190,16 @@
 ;; css-mode
 (setq css-indent-offset 2)
 ;;(add-to-list 'auto-mode-alist '("\\.scss$" . css-mode))
+
+;; graphviz mode
+(require 'graphviz-dot-mode)
+(add-hook 'graphviz-dot-mode-hook
+          (lambda()
+            (local-set-key [f5] "\C-x\C-s\C-cc\C-m")
+            (setq graphviz-dot-auto-indent-on-newline nil)
+            (setq graphviz-dot-auto-indent-on-semi nil)
+            (setq graphviz-dot-indent-width 2)
+            ))
 
 ;; scss-mode
 (require 'scss-mode)
