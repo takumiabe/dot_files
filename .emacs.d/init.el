73e30a820e424cecf8adb63856b8e95a20e4929c
Takumi Abe <abe@engraphia.com>
2015-06-06 03:33:39 +0900

    init-loader.el を導入

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,10 @@
   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
       (normal-top-level-add-subdirs-to-load-path)))
 
+(require 'init-loader)
+(setq init-loader-show-log-after-init nil)
+(init-loader-load "~/.emacs.d/inits")
+
 ; package manager
 (require 'package)
 (setq package-user-dir (expand-file-name "~/.emacs.d/packages"))
