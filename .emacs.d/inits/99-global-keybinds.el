3a2ff513cb3c3bc1f1e6dab13400595810f3c78a
Takumi Abe <abe@engraphia.com>
2015-06-10 21:00:37 +0900

    HOME/END系のキーバインドをlinuxからglobalに移動

--- .emacs.d/inits/99-global-keybinds.el
+++ .emacs.d/inits/99-global-keybinds.el
@@ -1,3 +1,10 @@
 ;; メール作成のキーバインドを殺す
 (define-key global-map "\C-xm" nil)
 
+;; [HOME] [END]の設定
+(define-key global-map "\M-[1~" 'beginning-of-line)
+(define-key global-map [select] 'end-of-line)
+(define-key global-map [prior] 'scroll-down-command)
+(define-key global-map [next] 'scroll-up-command)
+(define-key global-map [home] 'beginning-of-line)
+(define-key global-map [end] 'end-of-line)
