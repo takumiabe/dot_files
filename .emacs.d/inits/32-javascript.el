57fcb433cac9cb622ea799f4b34e115d904fefdf
Takumi Abe <abe@engraphia.com>
2016-01-15 22:22:33 +0900

    emacs: set js2-mode to .json

--- .emacs.d/inits/32-javascript.el
+++ .emacs.d/inits/32-javascript.el
@@ -4,6 +4,7 @@
 ;;(autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 (add-hook 'js2-mode-hook '(lambda ()
     (setq js2-basic-offset 2)
     (hs-minor-mode 1)))
