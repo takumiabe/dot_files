ac1e2b15f134097da77c9865f0035035de939701
Takumi Abe <abe@engraphia.com>
2016-01-15 22:27:26 +0900

    emacs: change objc-mode

---
+++ .emacs.d/inits/301-objc-mode.el
@@ -0,0 +1,15 @@
+(add-hook 'objc-mode-hook
+          '(lambda()
+             (setq c-basic-offset 4)
+             (setq tab-width 4)
+             (setq indent-tabs-mode nil)
+             (auto-complete-mode)
+             ))
+
+;; .hファイルもobjc-modeで開くけるようにする
+(add-to-list 'magic-mode-alist
+             `(,(lambda ()
+                  (and (string= (file-name-extension buffer-file-name) "h")
+                       (re-search-forward "@\\<interface\\>"
+                                          magic-mode-regexp-match-limit t)))
+               . objc-mode))
