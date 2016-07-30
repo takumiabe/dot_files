07a3ff7405bc44652e505d84e13aa7d6986b8570
Takumi Abe <abe@engraphia.com>
2016-07-30 12:39:03 +0900

    mac向けの表記をguard

--- .bashrc.d/git.bash
+++ .bashrc.d/git.bash
@@ -1,4 +1,7 @@
 
-source /usr/local/etc/bash_completion.d/git-prompt.sh
-source /usr/local/etc/bash_completion.d/git-completion.bash
+if [ -f /usr/local/etc/bash_completion.d ]; then
+    source /usr/local/etc/bash_completion.d/git-prompt.sh
+    source /usr/local/etc/bash_completion.d/git-completion.bash
+fi
+
 GIT_PS1_SHOWDIRTYSTATE=true
