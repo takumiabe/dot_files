97454c4a1757294ddec26cf4770ed6315bb97cda
Takumi Abe <abe@engraphia.com>
2016-07-30 12:37:54 +0900

    debianのプロンプト表記を統合

--- .bashrc.d/00init.bash
+++ .bashrc.d/00init.bash
@@ -5,6 +5,10 @@ if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
     . /etc/bash_completion
 fi
 
+if [ -f /etc/bash_completion ]; then
+    export PS1='[\[\033[01;32m\]\u@\h\[\033[01;33m\] \w\[\033[01;31m\]$(__git_ps1)\[\033[00m\]] \[\033[01;34m\]\$\[\033[00m\] '
+fi
+
 # and for macox
 if which brew > /dev/null; then
     if [ -f `brew --prefix`/etc/bash_completion ]; then
