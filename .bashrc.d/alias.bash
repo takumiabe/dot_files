bb9b88127ba5878c037bd589ae6232c9b71e2836
Takumi Abe <abe@engraphia.com>
2016-07-30 12:38:34 +0900

    debianのaliasを統合

--- .bashrc.d/alias.bash
+++ .bashrc.d/alias.bash
@@ -1,5 +1,7 @@
 
+alias l='ls -CF'
 alias ls='ls -G'
+alias la='ls -A'
 alias ll='ls -l'
 alias lll='ls -la'
 alias grep='grep --color=auto'
@@ -12,3 +14,7 @@ alias emacs="e"
 
 # SSH bgcolor (use $PRODUCTION_HOST)
 alias ssh='$HOME/dot_files/ssh-host-color'
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
