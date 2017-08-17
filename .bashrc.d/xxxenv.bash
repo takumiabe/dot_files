8319a1a9af81956fffb292b68a933c370ef6828e
Takumi Abe <abe@engraphia.com>
2017-08-17 18:55:29 +0900

    direnv, nodenvの初期化処理を追記

--- .bashrc.d/xxxenv.bash
+++ .bashrc.d/xxxenv.bash
@@ -2,6 +2,10 @@ if [ -d $HOME/.rbenv ]; then
     export PATH="$HOME/.rbenv/bin:$PATH"
 fi
 
+if which direnv > /dev/null; then
+  eval "$(direnv hook bash)"
+fi
+
 if which rbenv > /dev/null; then
     eval "$(rbenv init -)"
     export RUBY_BUILD_CACHE_PATH=$(rbenv root)/srcs
@@ -10,4 +14,8 @@ fi
 if which phpenv > /dev/null; then
     # 同居させるには、rbenvはphpenvよりも前にないといけないので末尾に。
     eval "$(phpenv init -)"
+fi
+
+if which nodenv > /dev/null; then
+  eval "$(nodenv init -)"
 fi
