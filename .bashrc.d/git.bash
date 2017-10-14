074ba73d375b96a7bcedb82d878f3b7aac3104d6
Takumi Abe <abe@engraphia.com>
2017-10-14 22:16:11 +0900

    bash: git用のPS1の設定をgit.bashに移動

--- .bashrc.d/git.bash
+++ .bashrc.d/git.bash
@@ -1,7 +1,17 @@
-
 if [ -f /usr/local/etc/bash_completion.d ]; then
     source /usr/local/etc/bash_completion.d/git-prompt.sh
     source /usr/local/etc/bash_completion.d/git-completion.bash
 fi
 
-GIT_PS1_SHOWDIRTYSTATE=true
+# git prompt
+if [[ "$(type -t __git_ps1)" = "function" ]]; then
+    export GIT_PS1_SHOWDIRTYSTATE=true
+
+    HOST="${BGreen}\u@\h${Color_Off}"
+    DIR="${BYellow}\w${Color_Off}"
+    GIT="${BRed}\$(__git_ps1)${Color_Off}"
+    P="${BBlue}\$${Color_Off}" # escape twice
+
+    export PS1="[${HOST} ${DIR}${GIT}]\n${P} "
+    unset HOST DIR GIT DOLLAR
+fi
