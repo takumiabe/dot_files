68b0a41567810a7867f8dc9924fa8c937832757f
Takumi Abe <abe@engraphia.com>
2019-11-06 18:28:56 +0900

    bash: goコマンドにpathが通ってない場合に通す

--- .bashrc.d/go.bash
+++ .bashrc.d/go.bash
@@ -1,4 +1,8 @@
-if which go > /dev/null; then
-  export PATH=$(go env GOPATH)/bin:$PATH
+if [[ -d /usr/local/go/bin ]]; then
+  export PATH=$PATH:/usr/local/go/bin
 fi
 
+if which go > /dev/null; then
+  export GOPATH=$HOME/.go
+  export PATH=$(go env GOPATH)/bin:$PATH
+fi
