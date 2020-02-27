0bc92e8bd35cc6718365882ca507f18558f099ea
Takumi Abe <abe@engraphia.com>
2020-02-27 11:55:45 +0900

    bash: pyenv

--- .bashrc.d/10xxxenv.bash
+++ .bashrc.d/10xxxenv.bash
@@ -34,6 +34,12 @@ if which goenv > /dev/null; then
     eval "$(goenv init -)"
 fi
 
+# TO install: curl https://pyenv.run | bash
+# via. https://github.com/pyenv/pyenv-installer
+if [[ -d $HOME/.pyenv ]]; then
+  export PATH="$HOME/.pyenv/bin:$PATH"
+fi
 if which pyenv > /dev/null; then
   eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
 fi
