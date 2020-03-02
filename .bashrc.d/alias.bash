5c07cffa52357bf3886944e2ae7a62513abda606
Takumi Abe <abe@engraphia.com>
2020-03-02 12:37:04 +0900

    bash: sayonara emacs

--- .bashrc.d/alias.bash
+++ .bashrc.d/alias.bash
@@ -8,10 +8,6 @@ alias grep='grep --color=auto'
 # alias sed='gsed'
 alias diff='colordiff'
 
-alias e="emacsclient -t -a ''"
-alias ee="emacsclient -e '(kill-emacs)'"
-alias emacs="e"
-
 # # SSH bgcolor (use $PRODUCTION_HOST)
 # alias ssh='$HOME/dot_files/ssh-host-color'
 
